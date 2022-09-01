9686228177c1b333bb1642ad3f83c4e81a53774c
= <=>
2022-09-01 12:24:25 -0500

    Version preliminar

---
+++ Unidad 1/Practica 6.py
@@ -0,0 +1,2 @@
+num=["cero","uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve","diez","once","doce","trece","catorce","quince","diediseis","diecisiete","dieciocho","diecinueve","veinte"]
+print(num[0])
