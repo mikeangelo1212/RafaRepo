ed0775d41240715048a8578a2ede4bc7b66a6615
= <=>
2022-09-01 13:02:31 -0500

    No me sale ajjaa

---
+++ Unidad 1/Practica 2.py
@@ -0,0 +1,15 @@
+#quitar los que son multiplo de 3
+l= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+x=len(l)#metemos la longitud de la lista pa que no se pase
+i=0
+b=-1 #b de borrar ajajaja
+
+while True:
+    b=b+3
+    l.pop(b)
+    b=b-1
+    i=i+1
+
+    if x>=15:
+        break
+print(l)
