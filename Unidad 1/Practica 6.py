b96a2ed8a4770205779b932071ae1abbf46ba411
= <=>
2022-09-01 12:29:00 -0500

    ya quedo

--- Unidad 1/Practica 6.py
+++ Unidad 1/Practica 6.py
@@ -1,2 +1,5 @@
+import os
+os.system("cls")
 num=["cero","uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve","diez","once","doce","trece","catorce","quince","diediseis","diecisiete","dieciocho","diecinueve","veinte"]
-print(num[0])
+i=input()#se puede hacer mas bonito pero si que si
+print(num[int(i)])
