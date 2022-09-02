133d14d5ebaa580c2ee71be2c40e417a3297a67d
= <=>
2022-09-02 08:51:56 -0500

    Practica 7 completada

--- Unidad 1/Practica 7.py
+++ Unidad 1/Practica 7.py
@@ -1,14 +1,15 @@
 import os
 from datetime import date
-import datetime
 
 os.system("cls")
 print("""Seleccione una opcion: 
 1.- Imprimir YYYY/MM/DD
 2.- Imprimir  MM/DD/YYYY""")
-tecla=input()
+tecla=int(input())
+x = date(date.today().year, date.today().month, date.today().day)
 if tecla == 1:
-    x = datetime.date(date.today().year, date.today().month, date.today().day)
+    print(x.strftime("%Y-%m-%d"))    
+elif tecla == 2:#elif es else if
+    print(x.strftime("%m-%d-%Y"))
 else:
-    x = datetime.date(date.today().month, date.today().day, date.today().year)
-print(x)
+    print("No existe esa opcion, byeeeeeeeeeeee")
