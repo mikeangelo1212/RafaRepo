5f2ce9b92dbe76daa632eff62c80f2c0099f938e
= <=>
2022-09-23 21:03:21 -0500

    completado

--- Unidad 1/Practica2.py
+++ Unidad 1/Practica2.py
@@ -1,15 +1,8 @@
-#quitar los que son multiplo de 3
-l= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-x=len(l)#metemos la longitud de la lista pa que no se pase
-i=0
-b=-1 #b de borrar ajajaja
-
-while True:
-    b=b+3
-    l.pop(b)
-    b=b-1
-    i=i+1
-
-    if x>=15:
-        break
+l =["A","B","C","D","E","F","G","H","I","J","K","L","M","N",
+    "O","P","Q","R","S","T","U","V","X","Y","Z"]
+for i in l:
+    f=0
+    if f%3 == 0:
+        l.remove(i)
+        f = f+1
 print(l)
